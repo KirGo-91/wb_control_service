1cf3b2eb089673ff81fb9d0058123d0e98fcfe5c
KirGo-91 <zenitfan91@yandex.ru>
2025-10-13 20:40:00 +0300

    modified

--- autocommit.sh
+++ autocommit.sh
@@ -7,7 +7,7 @@ cd /root/wb_control_service || exit
 git add .
 
 # Создание коммита с текущей датой и временем
-git commit -m "autocommit $(date '+%Y-%m-%d %H:%M')" || exit
+git commit -m "autocommit $(date '+%Y-%m-%d %H:%M')" || echo "No changes to commit"
 
 # Отправка изменений в удалённый репозиторий
 git push https://$GITHUB_TOKEN@github.com/KirGo-91/wb_control_service.git
